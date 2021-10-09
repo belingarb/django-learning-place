e69b60d00db3a35d66f0486f062d58cd9f77e915
belingarb <bozidar.belingar@gmail.com>
2021-10-09 13:19:01 +0200

    update

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="vjvaCtcOYvMyD1GoxUtGPspjXOY1qUmQL3Fr81gaUJYGQnXLUOkjnkA6ASd8RJHa",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
